87b52509aac24dbf520d316bd04ac0e59738cc25
keerthana1816 <keerthana1816@gmail.com>
2022-04-27 10:04:37 +0530

    Dress shop app

---
+++ src/main/java/com/dressshopapp/DressshopappApiApplication.java
@@ -0,0 +1,13 @@
+package com.dressshopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DressshopappApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DressshopappApiApplication.class, args);
+	}
+
+}
